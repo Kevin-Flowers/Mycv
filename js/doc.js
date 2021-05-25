cb1f1ec830764fe24418b58fd966cb2a21bb4609
Kevin Flores <84812264+Kevin-Flowers@users.noreply.github.com>
2021-05-25 17:02:34 -0500

    Add files via upload

---
+++ js/doc.js
@@ -0,0 +1,203 @@
+var inicio = document.getElementById("inicio");
+var dp = document.getElementById("dp");
+var mi = document.getElementById("mi"); 
+var el = document.getElementById("el");
+var ch = document.getElementById("ch");
+
+function dap(){
+   if (inicio.style.visibility === "visible"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+     dp.style.visibility = "visible";
+     inicio.style.visibility = "hidden";
+     mi.style.visibility = "hidden";
+     el.style.visibility = "hidden";
+     ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "visible" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+     dp.style.visibility = "visible";
+     inicio.style.visibility = "hidden";
+     mi.style.visibility = "hidden";
+     el.style.visibility = "hidden";
+     ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "visible" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+       dp.style.visibility = "visible";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "visible" && ch.style.visibility === "hidden") {
+       dp.style.visibility = "visible";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "visible") {
+       dp.style.visibility = "visible";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "hidden";}
+    else{
+        dp.style.visibility = "visible";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "hidden";
+    }
+    }
+
+function ini(){
+   if (inicio.style.visibility === "visible"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+     dp.style.visibility = "hidden";
+     inicio.style.visibility = "visible";
+     mi.style.visibility = "hidden";
+     el.style.visibility = "hidden";
+     ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "visible" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+       dp.style.visibility = "hidden";
+       inicio.style.visibility = "visible";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "visible" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+       dp.style.visibility = "hidden";
+       inicio.style.visibility = "visible";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "visible" && ch.style.visibility === "hidden") {
+       dp.style.visibility = "hidden";
+     inicio.style.visibility = "visible";
+     mi.style.visibility = "hidden";
+     el.style.visibility = "hidden";
+     ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "visible") {
+       dp.style.visibility = "hidden";
+     inicio.style.visibility = "visible";
+     mi.style.visibility = "hidden";
+     el.style.visibility = "hidden";
+     ch.style.visibility = "hidden";}
+     else{
+        dp.style.visibility = "hidden";
+       inicio.style.visibility = "visible";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "hidden";
+    }}
+
+function mini(){
+   if (inicio.style.visibility === "visible"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+     dp.style.visibility = "hidden";
+     inicio.style.visibility = "hidden";
+     mi.style.visibility = "visible";
+     el.style.visibility = "hidden";
+     ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "visible" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+     dp.style.visibility = "hidden";
+     inicio.style.visibility = "hidden";
+     mi.style.visibility = "visible";
+     el.style.visibility = "hidden";
+     ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "visible" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+     dp.style.visibility = "hidden";
+     inicio.style.visibility = "hidden";
+     mi.style.visibility = "visible";
+     el.style.visibility = "hidden";
+     ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "visible" && ch.style.visibility === "hidden") {
+     dp.style.visibility = "hidden";
+     inicio.style.visibility = "hidden";
+     mi.style.visibility = "visible";
+     el.style.visibility = "hidden";
+     ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "visible") {
+       dp.style.visibility = "hidden";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "visible";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "hidden";}
+       else{
+        dp.style.visibility = "hidden";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "visible";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "hidden";
+    }}
+
+function ele(){
+   if (inicio.style.visibility === "visible"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+     dp.style.visibility = "hidden";
+     inicio.style.visibility = "hidden";
+     mi.style.visibility = "hidden";
+     el.style.visibility = "visible";
+     ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "visible" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+       dp.style.visibility = "hidden";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "visible";
+       ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "visible" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+       dp.style.visibility = "hidden";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "visible";
+       ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "visible" && ch.style.visibility === "hidden") {
+     dp.style.visibility = "hidden";
+     inicio.style.visibility = "hidden";
+     mi.style.visibility = "hidden";
+     el.style.visibility = "visible";
+     ch.style.visibility = "hidden";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "visible") {
+       dp.style.visibility = "hidden";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "visible";
+       ch.style.visibility = "hidden";}
+       else{
+        dp.style.visibility = "hidden";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "visible";
+       ch.style.visibility = "hidden";
+    }}
+
+    function chu(){
+   if (inicio.style.visibility === "visible"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+     dp.style.visibility = "hidden";
+     inicio.style.visibility = "hidden";
+     mi.style.visibility = "hidden";
+     el.style.visibility = "hidden";
+     ch.style.visibility = "visible";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "visible" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+       dp.style.visibility = "hidden";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "visible";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "visible" && el.style.visibility === "hidden" && ch.style.visibility === "hidden") {
+       dp.style.visibility = "hidden";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "visible";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "visible" && ch.style.visibility === "hidden") {
+       dp.style.visibility = "hidden";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "visible";}
+   if (inicio.style.visibility === "hidden"  && dp.style.visibility === "hidden" && mi.style.visibility === "hidden" && el.style.visibility === "hidden" && ch.style.visibility === "visible") {
+       dp.style.visibility = "hidden";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "visible";}
+       else{
+        dp.style.visibility = "hidden";
+       inicio.style.visibility = "hidden";
+       mi.style.visibility = "hidden";
+       el.style.visibility = "hidden";
+       ch.style.visibility = "visible";
+    }}
+
+
